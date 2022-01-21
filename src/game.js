b21f38f37d467e2fd7e08dd974e44fbc93302aa9
Patrick Timm <pctimm60@gmail.com>
2022-01-20 22:46:42 -0300

    Installed prompt-sync via NPM

--- src/game.js
+++ src/game.js
@@ -2,6 +2,22 @@ let playerVictories = 0
 let aiVictories = 0
 
 console.log("Welcome to another game of JAN-KEN-PON!")
-while (true) {
+    const readline = require("readline");
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+async function readPlayerInput() {
+    return new Promise((resolve, reject) => {
+        const input = rl.question(">>> ", function (answer) {
+            rl.close();
+        });
+        
+        resolve(input)
+    })
 }
+const escolha = readPlayerInput()
+console.log("A sua escolha foi " + escolha)
+// function readPlay()
